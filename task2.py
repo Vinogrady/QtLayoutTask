c5989e3b6381ffe050feb9c8ff12e1235f465510
Vinogrady <132036574+Vinogrady@users.noreply.github.com>
2023-05-18 19:35:51 +1100

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,1 +1,26 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QHBoxLayout
+app = QApplication([])
+win = QWidget()
+win.show()
+
+main_layout = QHBoxLayout() 
+QV_layout = QVBoxLayout()
+
+b1 = QPushButton()
+b2 = QPushButton()
+b3 = QPushButton()
+b4 = QPushButton()
+b5 = QPushButton()
+
+main_layout.addWidget(b1)
+main_layout.addLayout(QV_layout)
+main_layout.addWidget(b2)
+QV_layout.addWidget(b3) 
+QV_layout.addWidget(b4)
+QV_layout.addWidget(b5)
+
+
+win.setLayout(main_layout) 
+app.exec()
 
